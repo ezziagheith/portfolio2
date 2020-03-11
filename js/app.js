319b26be71a890869acec09ef53401b656acb159
ezziagheith <ezziagheith@gmail.com>
2020-03-11 09:57:58 -0700

    Added first project

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$("#supreme-button").on('click', function(){
+    window.location = "https://supreme-emperor-react.herokuapp.com/"
+})
+
+$("#revive-button").on('click', function(){
+    window.location = "https://reviveandthrive.herokuapp.com/"
+})
