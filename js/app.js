4ed252273adaa0be351c8dcf7ba778d61f892807
ezziagheith <ezziagheith@gmail.com>
2020-03-12 10:02:53 -0700

    Third Project added

--- js/app.js
+++ js/app.js
@@ -5,3 +5,7 @@ $("#supreme-button").on('click', function(){
 $("#revive-button").on('click', function(){
     window.location = "https://reviveandthrive.herokuapp.com/"
 })
+
+$('#hoopers-button').on('click', function(){
+    window.location = "https://git.generalassemb.ly/ezziagheith/pickupgame"
+})
