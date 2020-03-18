ad06b62f31ca4a8cdd0df3dd9585a6655f4dd4ab
ezziagheith <ezziagheith@gmail.com>
2020-03-18 11:57:11 -0700

    About me section

--- js/app.js
+++ js/app.js
@@ -9,3 +9,19 @@ $("#revive-button").on('click', function(){
 $('#hoopers-button').on('click', function(){
     window.location = "https://git.generalassemb.ly/ezziagheith/pickupgame"
 })
+
+$('#whack-button').on('click', function(){
+    window.location = "https://pages.git.generalassemb.ly/ezziagheith/whackAFriend/"
+})
+
+$('#resume-button').on('click', function() {
+    window.location = "https://docs.google.com/document/d/1v2dEs_OKNk35tBMHVsGrW-4J_-L04iNUoon4nsDuke0/edit#heading=h.pjeyc6f9l6c6"
+})
+
+$('#github').on('click', function(){
+    window.location = "https://github.com/ezziagheith"
+})
+
+$('#linkedin').on('click', function(){
+    window.location = "https://www.linkedin.com/in/ezzia-gheith/"
+})
